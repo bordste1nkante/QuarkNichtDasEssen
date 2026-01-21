5674c7d66a00a912008436c544d3a582dc6e3968
ylkors <s6ylkors@cip-pool.physik.uni-bonn.de>
2026-01-21 09:39:44 +0100

    minor fixes and some questions

--- header/matrixOP.h
+++ header/matrixOP.h
@@ -10,7 +10,7 @@
 //generates identity and pauli matrices (base of complex 2x2 matrices)
 void generate_Pauli();
 
-//A*B
+//A*B A_ik B_kj = C_ij
 Matrix<rSU,cSU> matrix_multiplication(const Matrix<rSU,rSU>& A, const Matrix<rSU,rSU>& B);
 
 //A+B
@@ -20,7 +20,7 @@ Matrix<rSU,cSU> matrix_addition(const Matrix<rSU,rSU>& A, const Matrix<rSU,rSU>&
 Matrix<rSU,cSU> matrix_subtraction(const Matrix<rSU,rSU>& A, const Matrix<rSU,rSU>& B);
 
 
-// returns the complex conjugate of the matrix
+// returns the complex conjugate of the matrix -> but not the hermitian conjugate, i.e. conjugate & transpose
 Matrix<rSU,cSU> matrix_conjugate(const Matrix<rSU,rSU>& A);
 
 
--- src/matrixOP.cpp
+++ src/matrixOP.cpp
@@ -24,17 +24,19 @@ void generate_Pauli(){
             }
         }
     }
+    // this should be 0 & 1 as indices?
             Pauli2(1,2)=std::complex<double> {0.0,-1.0};
             Pauli2(2,1)=std::complex<double> {0.0,1.0};
             Pauli3(1,1)=std::complex<double> {1.0,0.0};
             Pauli3(2,2)=std::complex<double> {-1.0,0.0};
+            // was ist mit den off diagonal elementen für sigma_3? -> diagonalelemente für sigma_2?
 }
 
 Matrix<rSU,cSU> matrix_multiplication(const Matrix<rSU,rSU>& A, const Matrix<rSU,rSU>& B){
     Matrix<rSU,cSU> C;
     for(int i=0; i<rSU; i++){
         for(int j=0; j<cSU; j++){
-            std::complex<double> sum;
+            std::complex<double> sum;// ich check die funktion von diesem sum nicht
             for(int k = 0; k<rSU; k++){
                 sum += A(i,k)*B(k,j);
 
@@ -78,7 +80,7 @@ Matrix<rSU,cSU> matrix_conjugate(const Matrix<rSU,rSU>& A){
     for(int i=0; i<rSU; i++){
         for(int j=0; j<cSU; j++){
 
-            C(i,j)= std::conj(A(j,i));
+            C(i,j)= std::conj(A(j,i));//transponiert nicht?
 
             }
 
@@ -87,10 +89,10 @@ Matrix<rSU,cSU> matrix_conjugate(const Matrix<rSU,rSU>& A){
 }
 
 std::complex<double> matrix_trace(const Matrix<rSU,rSU>& A){
-    std::complex<double> trace;
+    std::complex<double> trace; //{0.0,0.0}; //-> muss ich dem Lachs nicht erst nen Wert zuweisen?
     for(int i=0; i<rSU; i++){
         for(int j=0; j<cSU; j++){
-            if(i=j){
+            if(i==j){//don't assign i to j
                 trace += A(i,j);
             }
 
